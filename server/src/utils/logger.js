4df40d2fbab0caf1a0bb0014e7656e08c1d9c33a
Allan Juma <allancjuma@gmail.com>
2024-06-14 16:16:38 +0300

    better loggin

--- server/src/utils/logger.js
+++ server/src/utils/logger.js
@@ -1,30 +1,35 @@
 const winston = require("winston");
+const path = require("path");
 
 const logger = winston.createLogger({
-  level: "debug", // Set to a lower level to capture more detailed logs
+  level: "debug",
   format: winston.format.combine(
-    winston.format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }), // Add timestamp
-    winston.format.errors({ stack: true }), // Log stack traces
-    winston.format.json(), // Log in JSON format
-    winston.format.printf(
-      (info) => `${info.timestamp} ${info.level}: ${info.message}`
-    ), // Print logs in this format
-    winston.format.colorize()
+    winston.format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
+    winston.format.errors({ stack: true }),
+    winston.format.json()
   ),
   transports: [
     new winston.transports.File({
-      filename: "logs/error.json",
+      filename: path.join("logs", "error.json"),
       level: "error",
     }),
     new winston.transports.Console({
       format: winston.format.combine(
-        winston.format.colorize(), // Add colors to console transport
-        winston.format.simple() // Simple format for console transport
+        winston.format.colorize(),
+        winston.format.simple()
       ),
     }),
   ],
   exceptionHandlers: [
-    new winston.transports.File({ filename: "logs/exceptions.json" }),
+    new winston.transports.File({
+      filename: path.join("logs", "exceptions.json"),
+    }),
+    new winston.transports.Console({
+      format: winston.format.combine(
+        winston.format.colorize(),
+        winston.format.simple()
+      ),
+    }),
   ],
 });
 

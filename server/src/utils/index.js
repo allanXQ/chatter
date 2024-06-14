158da0810ff324f2a2dd76447605e82edfd651b7
Allan Juma <allancjuma@gmail.com>
2024-06-14 16:18:17 +0300

    cleanup: files will be replaced

--- server/src/utils/index.js
+++ server/src/utils/index.js
@@ -2,7 +2,6 @@ const createId = require("./createId");
 const messages = require("../config/messages");
 const logger = require("./logger");
 const errorHOC = require("./errorHOC");
-const whatsappClient = require("./whatsappClient");
 const { clearTokens, generateTokens, setCookies } = require("./cookie");
 
 module.exports = {
@@ -13,5 +12,4 @@ module.exports = {
   clearTokens,
   generateTokens,
   setCookies,
-  whatsappClient,
 };

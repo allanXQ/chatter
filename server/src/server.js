78620c7e3d459562e4d366771b7c035a1dd69770
Allan Juma <allancjuma@gmail.com>
2024-06-14 16:18:39 +0300

    implroved security, error handling

--- server/src/server.js
+++ server/src/server.js
@@ -1,8 +1,31 @@
+require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const { DBConn } = require("./config");
+const cookieParser = require("cookie-parser");
+const { errorHandler } = require("./middleware");
 
 const app = express();
-app.disable("x-powered-by");
 
-app.listen(4000, () => {
-  console.log("Server listening on port 4000");
+// Middlewares
+app.use(cors());
+app.use(helmet());
+app.use(express.json());
+app.use(morgan("tiny"));
+app.use(cookieParser());
+
+// Base Route
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
+
+app.use("*", (req, res) => {
+  res.status(404).json({ message: messages.urlNotFound });
+});
+
+app.use(errorHandler);
+
+// Start server
+DBConn(app);

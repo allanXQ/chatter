e2e44fdb62c6b0d1e1e19964b29de99af1d154d4
Allan Juma <allancjuma@gmail.com>
2024-06-14 17:50:08 +0300

    update: session exports

--- server/src/services/whatsapp.js
+++ server/src/services/whatsapp.js
@@ -1,37 +1,53 @@
-const { Client } = require("whatsapp-web.js");
+const { Client, LocalAuth, MessageMedia } = require("whatsapp-web.js");
 const qrcode = require("qrcode-terminal");
-const Message = require("../models/Message");
+const fs = require("fs");
+const path = require("path");
+const log = require("./logger"); // Assuming you have a logger utility
 
-const client = new Client();
+const SESSION_FILE_PATH = path.join(__dirname, "session.json");
+let sessionCfg;
+
+if (fs.existsSync(SESSION_FILE_PATH)) {
+  sessionCfg = require(SESSION_FILE_PATH);
+}
+
+// Configuration options for the client
+const client = new Client({
+  authStrategy: new LocalAuth(),
+  puppeteer: { headless: true }, // Run puppeteer in headless mode
+  session: sessionCfg,
+});
 
 client.on("qr", (qr) => {
   qrcode.generate(qr, { small: true });
+  log.info("QR Code received, scan to log in.");
 });
 
 client.on("ready", () => {
-  console.log("WhatsApp client is ready!");
+  log.info("Client is ready!");
 });
 
+client.on("authenticated", (session) => {
+  log.info("Client is authenticated");
+  fs.writeFile(SESSION_FILE_PATH, JSON.stringify(session), (err) => {
+    if (err) {
+      log.error(err);
+    }
+  });
+});
+
+client.on("auth_failure", (msg) => {
+  log.error("Authentication failure", msg);
+  fs.unlinkSync(SESSION_FILE_PATH); // Remove the session file if authentication fails
+});
+
+client.on("disconnected", (reason) => {
+  log.info("Client was logged out", reason);
+  fs.unlinkSync(SESSION_FILE_PATH);
+  client.initialize(); // Reinitialize client
+});
+
+// Initialize the client
 client.initialize();
 
-const sendMessage = async (messageId) => {
-  let message;
-  try {
-    message = await Message.findById(messageId).populate("contact");
-    const { phoneNumber, content } = message;
-
-    const chatId = phoneNumber.includes("@c.us")
-      ? phoneNumber
-      : `${phoneNumber}@c.us`;
-    await client.sendMessage(chatId, content);
-
-    message.status = "sent";
-    await message.save();
-  } catch (error) {
-    console.error("Failed to send message:", error);
-    message.status = "failed";
-    await message.save();
-  }
-};
-
-module.exports = { sendMessage };
+module.exports = { client, MessageMedia };
